e08da27149e7e5b6409e1547bb5403e9549de8fb
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-03 15:50:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7368488558:AAFJZYQoRsnaf4v0UR2NlTRiIj6GnnbwnvY")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority")
-API_ID = int(os.getenv("API_ID", "0"))
+API_ID = int(os.getenv("API_ID", "9301087"))
 API_HASH = os.getenv("API_HASH", "cbabdb3f23de6326352ef3ac26338d9c")
 
 # Convert API_ID to int safely
