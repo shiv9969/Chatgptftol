1bb2725809bc5003aa78768cc0ca8e173e7b2da6
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-03 15:33:32 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
+API_ID = int(os.getenv("API_ID", "your_api_id"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
