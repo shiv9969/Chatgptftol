9d24974bebdd8f66d581474961ed31e59f3a7eff
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-03 15:45:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "7368488558:AAFJZYQoRsnaf4v0UR2NlTRiIj6Gnnbwn
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority")
 API_ID = int(os.getenv("API_ID", "9301087"))
 API_HASH = os.getenv("API_HASH", "cbabdb3f23de6326352ef3ac26338d9c")
+
+# Convert API_ID to int safely
+try:
+    API_ID = int(API_ID)
+except ValueError:
+    API_ID = 0  # Fallback to 0 if conversion fails
