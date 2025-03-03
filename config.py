00e4e65bce72f6f9bb2889bc95fd830e0b2c5a99
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-03 15:38:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-MONGO_URI = os.getenv("MONGO_URI", "your_mongo_uri")
-API_ID = int(os.getenv("API_ID", "your_api_id"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7368488558:AAFJZYQoRsnaf4v0UR2NlTRiIj6GnnbwnvY")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(os.getenv("API_ID", "9301087"))
+API_HASH = os.getenv("API_HASH", "cbabdb3f23de6326352ef3ac26338d9c")
